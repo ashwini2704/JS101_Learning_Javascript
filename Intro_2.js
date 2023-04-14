4b5a72974de5f6514c676193f0e5ca60d34c2a9e
ashwini2704 <130367207+ashwini2704@users.noreply.github.com>
2023-04-15 02:41:22 +0530

    Add files via upload

---
+++ Intro_2.js
@@ -0,0 +1,15 @@
+//Result
+
+let name= "Advika";
+let roll_no= 101;
+
+console.log("*******",`𝐑𝐞𝐩𝐨𝐫𝐭 𝐂𝐚𝐫𝐝`,"*******");
+console.log(`𝐶𝐸𝑁𝑇𝐸𝑅 𝑃𝑂𝐼𝑁𝑇 𝑆𝐶𝐻𝑂𝑂𝐿 𝑁𝐴𝐺𝑃𝑈𝑅`);
+console.log("Name - ",name);
+console.log("Roll_No - ",roll_no);
+
+console.log("*********MARKS OBTAINED*********");
+
+console.log("SCIENCE : 75");
+console.log("MATHEMATICS : 95");
+console.log("ENGLISH : 65");
